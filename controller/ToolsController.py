6fd0507ed2bb68861d763f017b6f1aa10745ac2c
Quentin L <dev@qqtinworks.fr>
2025-09-23 15:21:51 +0200

    Adding custom exceptions & first tool edit

--- controller/ToolsController.py
+++ controller/ToolsController.py
@@ -1,4 +1,4 @@
-from moviepy import VideoClip
+from moviepy import VideoClip, TextClip
 
 
 def cutVideo(video: VideoClip, cuttingTime: int, position='B'):
@@ -6,7 +6,7 @@ def cutVideo(video: VideoClip, cuttingTime: int, position='B'):
     videoDuration = video.duration
     if videoDuration <= cuttingTime:
         raise Exception('Cutting time cannot be higher than video duration.')
-
+    
     match position:
         case 'B':
             return video.subclipped(0, cuttingTime)
@@ -14,3 +14,14 @@ def cutVideo(video: VideoClip, cuttingTime: int, position='B'):
             return video.subclipped(videoDuration - cuttingTime, videoDuration)
         case _:
             raise Exception('Cutting position', position, 'is not valid.')
+
+
+def addingText(video: VideoClip, duration: int, text: str, position: str = 'center', fontsize: int = '16',
+               color: str = 'black'):
+    txtClip = TextClip(text=text, font_size=fontsize, color="white")
+    
+    # Say that you want it to appear for 10s at the center of the screen
+    txt_clip = txt_clip.with_position("center").with_duration(10)
+    
+    # Overlay the text clip on the first video clip
+    video = CompositeVideoClip([clip, txt_clip])
---
+++ controller/utils/VarConstraintChecker.py
@@ -0,0 +1,16 @@
+from Exceptions import ConstraintException
+
+####### NUMBERS #######
+def constraintPositiveNumber(number: int):
+	if number < 0:
+		raise ConstraintException('Number cannot be negative.')
+
+def constraintPositiveNumberInclusive(number: int):
+	if number <= 0:
+		raise ConstraintException('Number cannot be negative or zero.')
+	
+####### TEXT #######
+def constraintNotEmptyText(text: str):
+	if len(text.strip()) == 0:
+		raise ConstraintException('Text cannot be empty.')
+	
