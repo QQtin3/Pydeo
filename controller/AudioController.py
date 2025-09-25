44c6d7df2b3ca81c84bc02216d46c4c07956f8f7
Quentin L <dev@qqtinworks.fr>
2025-09-25 11:08:05 +0200

    New speed method

--- controller/AudioController.py
+++ controller/AudioController.py
@@ -5,5 +5,13 @@ from .utils.VarConstraintChecker import constraintPourcentageNumber
 
 
 def changeAudioVolume(video: VideoClip, volume: float) -> Clip:
+	"""Cut a part of a VideoClip & returns it
+
+		Args:
+			video (VideoClip): VideoClip object that will be cutted
+			volume (float): New volume to be used
+		Returns:
+	    	Clip: Clip with changed audio volume
+	"""
 	constraintPourcentageNumber(volume)
 	return video.with_volume_scaled(volume)
