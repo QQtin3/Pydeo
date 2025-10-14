07ffa258f0b58cedd4d365abd1dd7fee18265820
Synn95 <nathanalex53950@gmail.com>
2025-10-14 14:41:02 +0200

    Adds Timeline model and related classes
    Introduces the Timeline model to manage clips and their properties.
    
    Also introduces TimelineClip, TimelineVideoClip and TimelineAudioClip classes
    to represent individual clips on the timeline with video/audio specific properties.
    
    Moves the frames_to_timecode helper function from the QtEditorialTimelineWidget
    to VideoController.

--- model/Timeline.py
+++ model/Timeline.py
@@ -1,11 +1,23 @@
 from model.TimelineClip import TimelineClip
+from views.widgets.QtEditorialTimelineWidget import DEFAULT_CONSTANTS
 
 
 class Timeline:
+    name: str
+    height: int
     clips: list[TimelineClip]
     
-    def __init__(self) -> None:
-        pass
+    def __init__(self, name, height) -> None:
+        self.name = name
+        self.height = (
+            height 
+            if height is not None 
+            else DEFAULT_CONSTANTS["DEFAULT_TRACK_HEIGHT"]
+        )
+        self.clips = []  # list of ClipData
+
+    def add_clip(self, clip):
+        self.clips.append(clip)
     
     def addClip(self, file) -> None:
         # todo
--- model/TimelineClip.py
+++ model/TimelineClip.py
@@ -4,15 +4,15 @@ from controller.FileHandlerController import readAudioFile, readVideoFile
 from model.Effects import AudioEffect, VideoEffect
 
 class TimelineClip:
-    name: str
-    start: float
-    end: float
+    title: str
+    start_frame: float
+    duration_frames: int
     filePath: str 
     
-    def __init__(self, name: str, start: float, end: float, filePath: str):
-        self.name = name
-        self.start = start
-        self.end = end
+    def __init__(self, title: str, start_frame: float, duration_frames: int, filePath: str):
+        self.start_frame = start_frame
+        self.duration_frames = duration_frames
+        self.title = title
         self.filePath = filePath
         
 class TimelineVideoClip(TimelineClip):
@@ -21,8 +21,8 @@ class TimelineVideoClip(TimelineClip):
     fps: int
     effects: list[VideoEffect]
     
-    def __init__(self, name: str, start: float, end: float, filePath: str):
-        super().__init__(name, start, end, filePath)
+    def __init__(self, name: str, start_frame: float, duration_frame, filePath: str):
+        super().__init__(name, start_frame, duration_frame, filePath)
         
         self.videoClip, self.audioClip, self.fps = readVideoFile(self.filePath)
         
@@ -31,7 +31,7 @@ class TimelineAudioClip(TimelineClip):
     frequency: int
     effects: list[AudioEffect]
     
-    def __init__(self, name: str, start: float, end: float, filePath: str):
-        super().__init__(name, start, end, filePath)
+    def __init__(self, name: str, start: float, duration_frames: int, filePath: str):
+        super().__init__(name, start, duration_frames, filePath)
         
         self.audioClip, self.frequency = readAudioFile(self.filePath)
