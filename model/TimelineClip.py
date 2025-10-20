3c6e7eb2cd77ec480b80dec0a6a8b21307088e09
Ayette El homadi <ayetteelhomadi@yahoo.com>
2025-10-20 10:48:49 +0200

    Added duration in TimelineClip.py model

--- model/TimelineClip.py
+++ model/TimelineClip.py
@@ -8,13 +8,16 @@ class TimelineClip:
     start: float
     end: float
     filePath: str 
+    originalDuration: float
     
     def __init__(self, name: str, start: float, end: float, filePath: str):
         self.name = name
         self.start = start
         self.end = end
         self.filePath = filePath
-        
+        self.originalDuration = readVideoFile(filePath)[0].duration
+        self.duration = end - start
+
 class TimelineVideoClip(TimelineClip):
     videoClip: VideoClip
     audioClip: AudioClip | None
